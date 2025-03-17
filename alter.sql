bb428c3f86ebdcd8f470e424f5082a182a37db4a
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-03-17 01:08:36 -0700

    added a postgres alter

---
+++ alter.sql
@@ -0,0 +1,17 @@
+SELECT * from person2;
+
+
+ALTER Table person2
+    ADD COLUMN email VARCHAR(25) DEFAULT 'default@gmail.com' NOT NULL;
+ALTER Table person2
+    ALTER COLUMN user_age set NOT NULL;
+ALTER Table person2
+    ALTER COLUMN user_age DROP NOT NULL;
+ALTER Table person2
+    DROP COLUMN email ;
+ALTER Table person2
+    RENAME COLUMN age to user_age;
+ALTER Table person2
+  ALTER COLUMN user_name type VARCHAR(50);
+
+    INSERT INTO person2 values(8,'test',25)
